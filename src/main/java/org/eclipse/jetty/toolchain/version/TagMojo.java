6cb2f9ef649d1b4dabda070b49816c115f858088
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2016-12-20 07:21:53 -0700

    Refactoring :effective tag support into new :tag goal

--- src/main/java/org/eclipse/jetty/toolchain/version/TagMojo.java
+++ src/main/java/org/eclipse/jetty/toolchain/version/TagMojo.java
@@ -18,7 +18,10 @@
 
 package org.eclipse.jetty.toolchain.version;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -28,37 +31,55 @@ import org.eclipse.jetty.toolchain.version.issues.Issue;
 import org.eclipse.jetty.toolchain.version.issues.IssueComparator;
 
 /**
- * Process the commit log to produce output representing the changes
- * that would be applied to the VERSION.txt
+ * Produce a target/version-tag.txt which represents the changes
+ * for this particular release.  A file suitable to use as the git tag
+ * message body, in tagging the release.
  *
- * @goal effective
+ * @goal tag
  * @requiresProject true
  * @phase process-resources
  */
 @SuppressWarnings("unused")
-public class EffectiveMojo extends UpdateVersionTextMojo
+public class TagMojo extends UpdateVersionTextMojo
 {
+    /**
+     * The generated version-tag.txt file.
+     * <p/>
+     *
+     * @parameter property="version.tag.output.file" default="${project.build.directory}/version-tag.txt"
+     */
+    private File versionTagOutputFile;
+    
+    /**
+     * The genreated version-tag.txt header text
+     *
+     * @parameter property="version.tag.header" default="Tag for release."
+     */
+    private String versionTagHeader;
+    
     @Override
     protected void updateVersionText(VersionText versionText, Release rel, String updateVersionText, String priorTagId, String priorCommitId, String currentCommitId) throws MojoFailureException, IOException
     {
-        if (!hasCredentialsFile("effective"))
+        if (!hasCredentialsFile("tag"))
         {
             return; // skip
         }
-    
+        
         // List issues
         List<Issue> issues = new ArrayList<>();
         issues.addAll(rel.getIssues());
         Collections.sort(issues, new IssueComparator());
-    
-        System.out.printf("Update Version: %s%n", updateVersionText);
-        System.out.printf("Changes from %s [%s]%n", priorTagId, priorCommitId);
-        System.out.printf("          to %s%n", currentCommitId);
-        System.out.println();
-    
-        for (Issue issue : issues)
+        
+        try (FileWriter writer = new FileWriter(versionTagOutputFile);
+             PrintWriter out = new PrintWriter(writer))
         {
-            System.out.println(issue);
+            out.println(versionTagHeader);
+            out.println();
+            
+            for (Issue issue : issues)
+            {
+                out.println(issue);
+            }
         }
     }
 }
