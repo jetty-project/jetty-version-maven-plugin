ed8292776f25f6e9a2188dd10e8446710799ffad
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2019-11-26 09:17:04 -0600

    Minor tweak to VersionPattern regex.
    + Allowed to start with 1-9 (not 0).
    + Next digit can exist in 0 to 2 occurrences.
    
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- src/main/java/org/eclipse/jetty/toolchain/version/VersionPattern.java
+++ src/main/java/org/eclipse/jetty/toolchain/version/VersionPattern.java
@@ -33,8 +33,7 @@ public class VersionPattern
     public VersionPattern(String key)
     {
         this.key = key;
-        //String regex = key.replace("-","[- ]").replace("VERSION","([1-9]{1,3}\\.[0-9]{1,}[^ ]*)");
-        String regex = key.replace("-","[- ]").replace("VERSION","(\\d{1,}{1,3}\\.[0-9]{1,}[^ ]*)");
+        String regex = key.replace("-","[- ]").replace("VERSION","([1-9][0-9]{0,2}\\.[0-9]{1,}[^ ]*)");
         this.pat = Pattern.compile("^" + regex,Pattern.CASE_INSENSITIVE);
     }
 
